fbbf068d2a96ca7a8d04d640f3b18f83fb86cb41
Fahreiza <fahreizataura@gmail.com>
2024-11-08 20:47:11 +0700

    baru

--- Week 10/Ajax/koneksi.php
+++ Week 10/Ajax/koneksi.php
@@ -2,7 +2,7 @@
 define('HOST', 'localhost');
 define('USER', 'root');
 define('PASS', '');
-define('DB1', 'prakwebdb');
+define('DB1', 'crud');
 
 //buat koneksi
 $db1 = new mysqli(HOST, USER, PASS, DB1);
